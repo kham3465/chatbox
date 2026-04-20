fbf48a9c701a7d01dfdc65d2146fc0e3c47f4536
Nguyễn Gia Khảm <khamng@ftlvn.com.vn>
2026-04-20 21:34:26 +0700

    update chatbox

--- src/main/java/com/vn/nhom2/dto/request/RegisterRequest.java
+++ src/main/java/com/vn/nhom2/dto/request/RegisterRequest.java
@@ -2,6 +2,7 @@ package com.vn.nhom2.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
@@ -9,6 +10,7 @@ public class RegisterRequest {
     @NotBlank(message = "Name cannot be blank")
     private String name;
     @NotBlank(message = "Password cannot be blank")
+    @Size(min = 6, max = 6, message = "Password must be exactly 6 characters")
     private String password;
     @NotBlank(message = "Phone number cannot be blank")
     @Pattern(regexp = "^\\d{10}$", message = "Phone number must be 10 digits")
